a6c6b4243fa8ca16c5a20e4a69e0866b2e1c0cb8
claire_t <e.claire.thompson@gmail.com>
2020-06-15 11:06:24 -0400

    Cleaned up script using map function

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
 /*********************** GLOBAL VARIABLES *********************/
 
+/* Display Element Variables */
+
+let [storyForm, submitButton, story, playAgain] = ['story-form', 'submit-button', 'story', 'play-again'].map(id => document.getElementById(id));
+
 /* Input Variables */
 
-let storyForm = document.getElementById('story-form');
-let noun1 = document.getElementById('noun1');
-let place = document.getElementById('place');
-let noun2 = document.getElementById('noun2');
-let noun3 = document.getElementById('noun3');
-let noun4 = document.getElementById('noun4');
-let adjective1 = document.getElementById('adjective1');
-let verb1 = document.getElementById('verb1');
-let number = document.getElementById('number');
-let adjective2 = document.getElementById('adjective2');
-let bodyPart = document.getElementById('body-part');
-let verb2 = document.getElementById('verb2');
-let submitButton = document.getElementById('submit-button');
+let variables = [noun1, place, noun2, noun3, noun4, adjective1, verb1, number, adjective2, bodyPart, verb2] = ['noun1', 'place', 'noun2', 'noun3', 'noun4', 'adjective1', 'verb1', 'number', 'adjective2', 'body-part', 'verb2'].map(id => document.getElementById(id));
 
 /* Final Story Variables */
 
-let story = document.getElementById('story');
-let firstNoun = document.getElementById('first-noun');
-let placeName = document.getElementById('place-name');
-let secondNoun = document.getElementById('second-noun');
-let thirdNoun = document.getElementById('third-noun');
-let fourthNoun = document.getElementById('fourth-noun');
-let firstAdjective = document.getElementById('first-adjective');
-let firstVerb = document.getElementById('first-verb');
-let numberWord = document.getElementById('number-word');
-let secondAdjective = document.getElementById('second-adjective');
-let bodyWord = document.getElementById('body-word');
-let secondVerb = document.getElementById('second-verb');
-let playAgain = document.getElementById('play-again');
+let newVariables = [firstNoun, placeName, secondNoun, thirdNoun, fourthNoun, firstAdjective, firstVerb, numberWord, secondAdjective, bodyWord, secondVerb] = ['first-noun', 'place-name', 'second-noun', 'third-noun', 'fourth-noun', 'first-adjective', 'first-verb', 'number-word', 'second-adjective', 'body-word', 'second-verb'].map(id => document.getElementById(id));
 
 /* Story Functions */
 
@@ -43,31 +23,13 @@ function wordIn(input, output) {
 function reset() {
     storyForm.style.display = 'flex';
     story.style.display = 'none';
-    noun1.value = '';
-    place.value = '';
-    noun2.value = '';
-    noun3.value = '';
-    noun4.value = '';
-    adjective1.value = '';
-    verb1.value = '';
-    number.value = '';
-    adjective2.value = '';
-    bodyPart.value = '';
-    verb2.value = '';
+    variables.map(word => word.value = '');
 };
 
 function makeStory() {
-    wordIn(noun1, firstNoun);
-    wordIn(place, placeName);
-    wordIn(noun2, secondNoun);
-    wordIn(noun3, thirdNoun);
-    wordIn(noun4, fourthNoun);
-    wordIn(adjective1, firstAdjective);
-    wordIn(verb1, firstVerb);
-    wordIn(number, numberWord);
-    wordIn(adjective2, secondAdjective);
-    wordIn(bodyPart, bodyWord);
-    wordIn(verb2, secondVerb);
+    for (i = 0; i < variables.length; i++) {
+        wordIn(variables[i], newVariables[i]);
+    };
     storyForm.style.display = 'none';
     story.style.display = 'block';
 };
